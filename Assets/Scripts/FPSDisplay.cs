09e8ffa9696eef78b92fa0c96ef692c7707b9a41
Prouser123 <jcahill773@gmail.com>
2018-09-25 07:25:15 +0100

    FPS Display

---
+++ Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour {
+
+    private Text fpsDisplay;
+    private int avgFPS;
+
+    void Start () {
+        fpsDisplay = GetComponent<Text>();
+    }
+
+    void Update () {
+        float current = 0;
+        //current = (int)(1f / Time.unscaledDeltaTime);
+        current = Time.frameCount / Time.time;
+        avgFPS = (int)current;
+        fpsDisplay.text = avgFPS.ToString() + " FPS";
+	}
+}
